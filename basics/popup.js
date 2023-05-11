426db9274a0c2d3f84815adbff9a616b821a29e2
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-05-10 21:17:21 -0500

    Basic Extension that shows time

---
+++ basics/popup.js
@@ -0,0 +1,10 @@
+const timeElement = document.getElementById("time")
+const currentTime = new Date().toLocaleTimeString()
+
+timeElement.textContent = `The time is: ${currentTime}`
+
+chrome.action.setBadgeText({
+  text: "TIME"
+}, () => { 
+  console.log('Finished Setting Badge Test')
+})
