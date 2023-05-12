af96378a20adde832a7fb0cd9c88da5fd837b705
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-05-12 18:22:58 -0500

    Setup timer with HTML popup updates

--- basics/popup.js
+++ basics/popup.js
@@ -2,13 +2,19 @@ const timeElement = document.getElementById("time");
 const nameElement = document.getElementById("name");
 const timerElement = document.getElementById("timer");
 
-const currentTime = new Date().toLocaleTimeString();
-timeElement.textContent = `The time is: ${currentTime}`;
+// Setting the time setting function to activate on HTML popup every 1 sec
+function updateTimeElements() {
+  const currentTime = new Date().toLocaleTimeString();
+  timeElement.textContent = `The time is: ${currentTime}`;
 
-chrome.storage.local.get(["timer"], (res) => {
-  const time = res.timer ?? 0;
-  timerElement.textContent = `The Timer is at: ${time}`;
-});
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = res.timer ?? 0;
+    timerElement.textContent = `The Timer is at: ${time}`;
+  });
+}
+
+updateTimeElements();
+setInterval(updateTimeElements, 1000);
 
 chrome.storage.sync.get(["name"], (res) => {
   const name = res.name ?? "??";
