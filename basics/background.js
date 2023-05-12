792da90b647409fa2b1bce5ee34495770a60d132
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-05-12 18:20:56 -0500

    Timer setup in background

--- basics/background.js
+++ basics/background.js
@@ -1,28 +1,21 @@
-chrome.alarms.create({
-  periodInMinutes: 1 / 60,
-})
+// Creating alarm that will run once a second
+// other options like delays can be set
+chrome.alarms.create("timerAlarm", {
+  periodInMinutes: 1 / 60, // Alarm will run every second
+});
 
+// Alarm listener
 chrome.alarms.onAlarm.addListener((alarm) => {
-  chrome.storage.local.get(["timer", "isRunning"], (res) => {
-    const time = res.timer ?? 0
-    const isRunning = res.isRunning ?? true
-    if (!isRunning) {
-      return
-    }
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = res.timer ?? 0;
+    // Increasing the timer state
     chrome.storage.local.set({
       timer: time + 1,
-    })
+    });
+
+    // Settting the badgeText
     chrome.action.setBadgeText({
-      text: `${time + 1}`
-    })
-    chrome.storage.sync.get(["notificationTime"], (res) => {
-      const notificationTime = res.notificationTime ?? 1000
-      if (time % notificationTime == 0) {
-        this.registration.showNotification("Chrome Timer Extension", {
-          body: `${notificationTime} seconds has passed!`,
-          icon: "icon.png",
-        })
-      }
-    })
-  })
-})
+      text: `${time + 1}`,
+    });
+  });
+});
--- basics/popup.js
+++ basics/popup.js
@@ -1,10 +1,16 @@
-const timeElement = document.getElementById("time")
-const currentTime = new Date().toLocaleTimeString()
+const timeElement = document.getElementById("time");
+const nameElement = document.getElementById("name");
+const timerElement = document.getElementById("timer");
 
-timeElement.textContent = `The time is: ${currentTime}`
+const currentTime = new Date().toLocaleTimeString();
+timeElement.textContent = `The time is: ${currentTime}`;
 
-chrome.action.setBadgeText({
-  text: "TIME"
-}, () => { 
-  console.log('Finished Setting Badge Test')
-})
+chrome.storage.local.get(["timer"], (res) => {
+  const time = res.timer ?? 0;
+  timerElement.textContent = `The Timer is at: ${time}`;
+});
+
+chrome.storage.sync.get(["name"], (res) => {
+  const name = res.name ?? "??";
+  nameElement.textContent = `Your name is: ${name}`;
+});
